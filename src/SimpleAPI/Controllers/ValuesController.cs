fe18b2d75c3f03b6cb2c915714da502b2d3e377d
Tushar Mehere <tusha@MININT-C6PHA3F.redmond.corp.microsoft.com>
2020-05-26 19:28:06 -0700

    Initial commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "something else";
+        }
+    }
+}
